77427bf6dba0d84217fd6c5e75eed56808339c7c
七画一只妖 <1157529280@qq.com>
2022-10-14 18:10:40 +0800

    init

---
+++ handler.py
@@ -0,0 +1,10 @@
+'''
+Author: 七画一只妖 1157529280@qq.com
+Date: 2022-10-14 16:25:19
+LastEditors: 七画一只妖 1157529280@qq.com
+LastEditTime: 2022-10-14 18:10:40
+FilePath: \QsPilUtils\handler.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+from PIL import Image, ImageFont, ImageDraw, ImageMath
+
