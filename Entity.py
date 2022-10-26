169c9ca14bf14a2bc4859de7eeadde83d3672f3a
七画一只妖 <1157529280@qq.com>
2022-10-26 22:45:50 +0800

    fix

--- Entity.py
+++ Entity.py
@@ -2,7 +2,7 @@
 Author: 七画一只妖 1157529280@qq.com
 Date: 2022-10-24 22:04:48
 LastEditors: 七画一只妖 1157529280@qq.com
-LastEditTime: 2022-10-24 22:15:02
+LastEditTime: 2022-10-26 22:45:38
 FilePath: \QsPilUtils\Entity.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -11,11 +11,6 @@ BASE_PATH:str = Path(__file__).absolute().parents[0]
 
 
 class FontEntity:
-    fsize = None
-    color = None
-    ttf_path = None
-
-
     def __init__(self, fsize: int = 12, color: str = "#000000", ttf_path: str = str(Path(BASE_PATH)/r"ttf"/r"zh-cn.ttf")) -> None:
         self.fsize = fsize
         self.color = color
