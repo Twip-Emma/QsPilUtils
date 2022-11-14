cbcb3e1fdc195f04012f292a8a4f5accd080aa05
七画一只妖 <1157529280@qq.com>
2022-11-14 22:32:51 +0800

    设计工厂

--- handler.py
+++ handler.py
@@ -2,7 +2,7 @@
 Author: 七画一只妖 1157529280@qq.com
 Date: 2022-10-14 16:25:19
 LastEditors: 七画一只妖 1157529280@qq.com
-LastEditTime: 2022-11-13 10:11:04
+LastEditTime: 2022-11-14 18:15:18
 FilePath: \QsPilUtils\handler.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -121,4 +121,6 @@ def write_longsh(font_entity: FontEntity, bg: BackgroundImage, text: str, mode: 
 
 
 class SolidImageFactory:
-    pass
+    def __init__(self, font: FontEntity) -> None:
+        self.font: FontEntity = font
+        
